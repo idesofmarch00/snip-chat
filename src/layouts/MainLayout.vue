5287dfc4a0bad7e5915ed8a6580707af6cd00ffa
Sahil Ahmed <sahil.ahmed@fuelbuddy.in>
2023-04-01 19:21:19 +0530

    basic list items in drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,8 +12,82 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" side="left" overlay elevated>
-      profile edit and settings privacy policy logout
+    <q-drawer dark :width="200" v-model="leftDrawerOpen" side="left" overlay elevated>
+      <q-list bordered padding class="rounded-borders text-primary">
+        <q-item
+          clickable
+          v-ripple
+          :active="link === 'profile'"
+          @click="link = 'profile'"
+          active-class="my-menu-link"
+        >
+          <q-item-section avatar>
+            <q-icon name="person" />
+          </q-item-section>
+
+          <q-item-section>Profile</q-item-section>
+        </q-item>
+
+        <q-item
+          clickable
+          v-ripple
+          :active="link === 'about'"
+          @click="link = 'about'"
+          active-class="my-menu-link"
+        >
+          <q-item-section avatar>
+            <q-icon name="info" />
+          </q-item-section>
+
+          <q-item-section>About</q-item-section>
+        </q-item>
+
+        <q-item
+          clickable
+          v-ripple
+          :active="link === 'logout'"
+          @click="link = 'logout'"
+          active-class="my-menu-link"
+        >
+          <q-item-section avatar>
+            <q-icon
+              name="logout"
+            />
+          </q-item-section>
+
+          <q-item-section>Trash</q-item-section>
+        </q-item>
+
+        <q-separator spaced />
+
+        <q-item
+          clickable
+          v-ripple
+          :active="link === 'settings'"
+          @click="link = 'settings'"
+          active-class="my-menu-link"
+        >
+          <q-item-section avatar>
+            <q-icon name="settings" />
+          </q-item-section>
+
+          <q-item-section>Settings</q-item-section>
+        </q-item>
+
+        <q-item
+          clickable
+          v-ripple
+          :active="link === 'help'"
+          @click="link = 'help'"
+          active-class="my-menu-link"
+        >
+          <q-item-section avatar>
+            <q-icon name="help" />
+          </q-item-section>
+
+          <q-item-section>Help</q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-page-container>
@@ -36,18 +110,26 @@
 
 <script setup lan="ts">
 import { useRoute } from 'vue-router';
-import { ref,onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 
 const leftDrawerOpen = ref(false);
 const tab = ref('chat');
 
 const route = useRoute();
+const link = ref('');
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 
 onMounted(() => {
-  console.log(route.name)
+  console.log(route.name);
 });
 </script>
+
+<style scoped lang="sass">
+.my-menu-link
+  color: white
+  background: #fffff
+
+</style>
