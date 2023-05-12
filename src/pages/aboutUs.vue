7eacc93bd5edee9744a5c929dc93fcf4e73f28ab
Sahil Ahmed <sahil.ahmed@fuelbuddy.in>
2023-05-12 11:01:49 +0530

    aboutUs page

--- src/pages/aboutUs.vue
+++ src/pages/aboutUs.vue
@@ -1,34 +1,32 @@
 <template>
   <q-page class="q-pa-md">
     <div class="q-pa-md q-gutter-md">
-      <!-- <p>
-    Welcome to our real-time chatting progressive web app, built with the latest
-    Quasar framework! Our app offers an unparalleled chatting experience with
-    features that make communication easier and more engaging than ever before.
-    Our platform allows users to share media, including disappearing photos, and
-    location in real-time. This makes it perfect for staying in touch with
-    friends, family, and colleagues, whether you're on the go or at home. Our
-    app also offers a user-friendly interface that allows for easy login and
-    registration. We understand the importance of keeping your information
-    secure and private, so we have implemented top-of-the-line security measures
-    to ensure your safety. Whether you're using our app for personal or
-    professional purposes, you'll find that our features are perfect for staying
-    connected with others. We're committed to providing our users with the best
-    experience possible, so we're always improving and updating our app to meet
-    their needs. Thank you for choosing our real-time chatting progressive web
-    app. We look forward to helping you stay connected with the people that
-    matter most. - by Sahil Ahmed </p> -->
-      <q-card>
+      <p class="text-lg">
+        This app offers an unparalleled chatting experience with features that
+        make communication easier and more engaging than ever before. Our
+        platform allows users to share media, including disappearing photos, and
+        location through map in real-time as well as a user-friendly interface
+        that allows for easy login and registration. We understand the
+        importance of keeping your information secure and private, so we do not
+        share your data with any third party. We're committed to providing our
+        users with the best experience possible, so we're always improving and
+        updating our app.
+      </p>
+      <q-card class="flex items-center justify-center">
         <q-card-section>
           <div class="text-h6">Our Company</div>
           <div class="text-body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod sem et enim bibendum vestibulum. Duis efficitur magna eget tellus finibus, vel pulvinar justo porttitor. Nullam vel faucibus ex. Fusce at ante ut lorem tristique lacinia. Maecenas consequat dictum ultricies.
+            <q-avatar size="100px">
+              <img src="../assets/company.png" />
+            </q-avatar>
           </div>
         </q-card-section>
         <q-card-section>
           <div class="text-h6">Our Team</div>
           <div class="text-body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod sem et enim bibendum vestibulum. Duis efficitur magna eget tellus finibus, vel pulvinar justo porttitor. Nullam vel faucibus ex. Fusce at ante ut lorem tristique lacinia. Maecenas consequat dictum ultricies.
+            <q-avatar size="100px">
+              <img src="../assets/me.png" />
+            </q-avatar>
           </div>
         </q-card-section>
       </q-card>
